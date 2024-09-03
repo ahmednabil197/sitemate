2917f976088d32ea7af71b2c13dda6a04d915665
Ahmed Nabil <ahmed.nabil@pncl.tech>
2024-09-03 13:16:59 +0300

    added findone api

--- server/src/api/issues/issues.controller.ts
+++ server/src/api/issues/issues.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { IssueClass, IssueClassWithId, IssueModel } from "./issue.model";
+import IdParam from "../interfaces/idParam";
 
 export async function findAll(req: Request, res: Response, next: NextFunction) {
   res.json({
@@ -7,17 +8,24 @@ export async function findAll(req: Request, res: Response, next: NextFunction) {
   });
 }
 
-export async function findOne(req: Request, res: Response, next: NextFunction) {
-  res.json({
-    message: 'ack fine one!',
-  });
+export async function findOne(req: Request<IdParam>, res: Response<IssueClassWithId>, next: NextFunction) {
+  try {
+    const result = await IssueModel.findById(req.params.id)
+    if (!result) {
+        res.status(404);
+        throw new Error(`Issue with id "${req.params.id}" not found.`);
+      }
+    res.json(result);
+  } catch(error) {
+    next(error);
+  }
 }
 
 export async function createOne(req: Request<{}, {}, IssueClass>, res: Response<IssueClassWithId>, next: NextFunction) {
   try {
-      const result = await IssueModel.create(req.body);
-      
-      res.status(201).json(result.toJSON())
+    const result = await IssueModel.create(req.body);
+    
+    res.status(201).json(result.toJSON())
   } catch(error) {
       next(error)
   }
--- server/src/middlewares.ts
+++ server/src/middlewares.ts
@@ -3,21 +3,22 @@ import mongoose from "mongoose";
 import ErrorResponse from "./api/interfaces/ErrorResponse";
 
 export function notFound(req: Request, res: Response, next: NextFunction) {
-    res.status(404);
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    next(error);
+  res.status(404);
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  next(error);
 }
 
 export function errorHandler(error: Error, req: Request, res: Response<ErrorResponse>, next: NextFunction) {
-    if (res.statusCode === 200) {
-        if(error instanceof mongoose.Error.ValidationError) {
-            res.statusCode = 422;
-        }
-    }
-    const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
-    res.status(statusCode);
-    res.json({
-      message: error.message,
-      stack: process.env.NODE_ENV === 'production' ? 'stack trace not availble on production' : error.stack,
-    });
+  if (res.statusCode === 200) {
+      if(error instanceof mongoose.Error.ValidationError || 
+          error instanceof mongoose.Error.CastError && error.kind === 'ObjectId') {
+          res.statusCode = 422;
+      }
+  }
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? 'stack trace not availble on production' : error.stack,
+  });
 }
